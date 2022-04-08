55ed07e496318cff68e0a6ec6f825f316e84f6c8
Dominic Couture <45298797+mastermind-mayhem@users.noreply.github.com>
2022-04-08 17:55:55 -0400

    Update main.py

--- main.py
+++ main.py
@@ -41,22 +41,6 @@ popular_websites = {
     "github": "https://www.github.com",
 }
 
-romeo = """
-Two households, both alike in dignity,
-In fair Verona, where we lay our scene,
-From ancient grudge break to new mutiny,
-Where civil blood makes civil hands unclean.
-From forth the fatal loins of these two foes
-A pair of star-cross'd lovers take their life;
-Whose misadventured piteous overthrows
-Do with their death bury their parents' strife.
-The fearful passage of their death-mark'd love,
-And the continuance of their parents' rage,
-Which, but their children's end, nought could remove,
-Is now the two hours' traffic of our stage;
-The which if you with patient ears attend,
-What here shall miss, our toil shall strive to mend.
-"""
 
 def main(search_engine, take_command, debug):
     def execute_the_command_said_by_user():
